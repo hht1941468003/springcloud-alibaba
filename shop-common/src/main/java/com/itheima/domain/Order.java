272514902b5cf5306d43b50567662d20d25a3146
hehaitao <1941468003@qq.com>
2021-08-21 20:43:31 +0800

    222

--- shop-common/src/main/java/com/itheima/domain/Order.java
+++ shop-common/src/main/java/com/itheima/domain/Order.java
@@ -11,5 +11,19 @@ import javax.persistence.Id;
 @Entity(name = "shop_order")
 @Data
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long oid;//订单id
 
+    //用户
+    private Integer uid;//用户id
+    private String username;//用户名
+
+    //商品
+    private Integer pid;//商品id
+    private String pname;//商品名称
+    private Double pprice;//商品单价
+
+    //数量
+    private Integer number;//购买数量
 }
